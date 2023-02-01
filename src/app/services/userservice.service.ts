dc6bf3bdad60372c7a548dc7bc673cffad401f6b
Rosa Rodríguez <angierodriguezd95@gmail.com>
2023-02-01 17:58:23 -0500

    init

---
+++ src/app/services/userservice.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Users } from '../models/users';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserserviceService {
+
+  // Crear una instancia de HttpClient
+  constructor(private http: HttpClient) { }
+
+  // Definir la URL (EndPoint) de la API (La BaseURL de la API estara en environment.ts)
+  apiVista = environment.apiURL + 'vista/';
+  apiEliminar = environment.apiURL + 'eliminar/';
+  apiActualizar = environment.apiURL + 'actualizar/';
+  apiAgregar = environment.apiURL + 'nuevo/';
+  // Metodos GET para obtener datos de la API users
+
+  getUsersAll():Observable<Users[]>{
+    return this.http.get<Users[]>(this.apiVista);
+  }
+
+  getUserId(userId: string): Observable<Users> {
+    const url = this.apiVista + userId;
+    return this.http.get<Users>(url);
+  }
+
+  // Metodo POSt para enviar datos a la API users
+
+  postUser(usuario: Users): Observable<any>{
+    return this.http.post<any>(this.apiAgregar, usuario);
+  }
+
+  editUser(usuario: Users): Observable<Users>{
+    return this.http.put<Users>(this.apiVista, usuario);
+  }
+
+  deleteUser(userId: string): Observable<Users>{
+    const url = this.apiEliminar + userId;
+    const resp = this.http.delete<Users>(url);
+    return resp;
+  }
+
+  // Metodo GET para el Interceptor
+  getUsersAllInterceptor():Observable<any>{
+    return this.http.get(this.apiVista, {observe: 'response'});
+  }
+}
